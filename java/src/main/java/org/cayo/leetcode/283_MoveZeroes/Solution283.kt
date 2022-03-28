077c0a3a8f60699066730fed7139db8788d5b61b
Cayo Amorim <cmesquita@quotemedia.com>
2022-03-28 09:22:48 -0700

    Add leetCode 283 move zeros

---
+++ java/src/main/java/org/cayo/leetcode/283_MoveZeroes/Solution283.kt
@@ -0,0 +1,25 @@
+package org.cayo.leetcode.`283_MoveZeroes`
+
+class Solution283 {
+    fun moveZeroes(nums: IntArray): Unit {
+        var firstZero = -1
+        for (i in nums.indices) {
+            val num = nums[i]
+            if ( num == 0 ) {
+                if (firstZero < 0) {
+                    firstZero = i
+                }
+            } else {
+                if (firstZero >= 0) {
+                    swap(nums, i, firstZero++)
+                }
+            }
+        }
+    }
+
+    fun swap(nums: IntArray, i: Int, j: Int) {
+        val aux = nums[i]
+        nums[i] = nums[j]
+        nums[j] = aux
+    }
+}
