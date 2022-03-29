244c9e4c58f34bfa3ec73bbdefdd38d82122ca6e
Cayo Amorim <cmesquita@quotemedia.com>
2022-03-29 08:14:43 -0700

    Add leetCode 167 Two sum II - Input array is sorted

---
+++ java/src/main/java/org/cayo/leetcode/344_ReverseString/Solution344.kt
@@ -0,0 +1,17 @@
+package org.cayo.leetcode.`344_ReverseString`
+
+class Solution344 {
+    fun reverseString(s: CharArray): Unit {
+        var left = 0
+        var right = s.size - 1
+        while (left < right){
+            swap(s, left++, right--)
+        }
+    }
+
+    fun swap(s: CharArray, a:Int, b:Int) {
+        val aux = s[a]
+        s[a] = s[b]
+        s[b] = aux
+    }
+}
