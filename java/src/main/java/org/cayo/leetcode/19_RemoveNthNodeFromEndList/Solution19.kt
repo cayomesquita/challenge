bab44f8c13f2aaab5d18adff6c7326d45e30b6d7
Cayo Amorim <cmesquita@quotemedia.com>
2022-04-13 17:03:41 -0300

    Add leetCode 19. Remove Nth Node From End of List

---
+++ java/src/main/java/org/cayo/leetcode/19_RemoveNthNodeFromEndList/Solution19.kt
@@ -0,0 +1,25 @@
+package org.cayo.leetcode.`19_RemoveNthNodeFromEndList`
+
+import org.cayo.leetcode.common.ListNode
+import java.util.LinkedList
+
+class Solution19 {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        var list = LinkedList<ListNode>()
+        var node = head
+        while (node != null) {
+            if (list.size > n) {
+                list.pollLast()
+            }
+            list.push(node)
+            node = node.next
+        }
+        if (list.size == n) {
+            return head?.next
+        }
+        val aux = list.pollLast()
+        aux.next = aux.next?.next
+        return head
+    }
+
+}
