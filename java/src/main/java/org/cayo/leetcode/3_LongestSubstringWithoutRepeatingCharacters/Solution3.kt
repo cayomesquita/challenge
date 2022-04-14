36f91e6ab419bb65d77c8f915b016fc1298bd7b2
Cayo Amorim <cmesquita@quotemedia.com>
2022-04-14 18:52:48 -0300

    Add leetCode 3. Longest Substring Without Repeating Characters

---
+++ java/src/main/java/org/cayo/leetcode/3_LongestSubstringWithoutRepeatingCharacters/Solution3.kt
@@ -0,0 +1,19 @@
+package org.cayo.leetcode.`3_LongestSubstringWithoutRepeatingCharacters`
+
+class Solution3 {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var maximum: Int = 0
+        var start: Int = 0
+        var charPositions = HashMap<Char, Int>()
+        val arr = s.toCharArray()
+        for (i in arr.indices) {
+            val position = charPositions.put(arr[i], i)
+            if (position != null && position >= start) {
+                start = position + 1
+            }
+            val length = i - start + 1
+            maximum = Math.max(maximum, length)
+        }
+        return maximum
+    }
+}
